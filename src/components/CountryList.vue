0880473d858865ecd1e3afcdd650b4e5e524dca5
Andrew Binnie <binnieandrew@gmail.com>
2019-12-18 15:39:03 +0000

    adding the display of country name list

---
+++ src/components/CountryList.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+  <ul>
+    <li v-for="(country, index) in allCountries" :country="country" :key="index">{{country.name}}</li>
+  </ul>
+    </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'country-list',
+  props: ['allCountries'],
+
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
