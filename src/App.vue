7ce8248939a66c4ca7bf6f9040994636c0326ea3
Andrew Binnie <binnieandrew@gmail.com>
2019-12-18 14:56:28 +0000

    adding initial running app

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,12 @@ export default {
       countries: [],
       selectedCountry: null
     };
+
+  },
+  mounted(){
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(response => response.json())
+    .then(countries => this.countries = countries)
   }
   }
 
